f4b3605566dd87321787e37d3097e9c675366540
Gareth O'Sullivan <gareth.osullivan@icloud.com>
2017-10-26 20:52:39 +0100

    Added Hit Me button

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -17,6 +17,13 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome!", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 
 }
