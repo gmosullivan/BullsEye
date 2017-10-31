95b9b2f416f285165184c0ae3b18d12310321f45
Gareth O'Sullivan <gareth.osullivan@icloud.com>
2017-10-31 21:03:21 +0000

    Added Slider functionality

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,20 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app.", preferredStyle: .alert)
+        let message = "The value of the slider is \(currentValue)" + "\nThe Target Value is \(targetValue)"
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome!", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
 }
